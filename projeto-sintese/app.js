0fec9c1edf95d27f7425ce845da3459591352ec0
Nicolas Risso <105318465+NicolasRisso@users.noreply.github.com>
2022-07-05 17:11:20 -0300

    formulario / comeco 2

---
+++ projeto-sintese/app.js
@@ -0,0 +1,8 @@
+const nodemailer = require('nodemailer')
+const {google} = require('googleapis')
+
+const CLIENT_ID = ''
+const CLIENT_SECRET = ''
+const REDIRECT_URL = ''
+const REFRESH_TOKEN = ''
+
